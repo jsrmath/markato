417597afec9b6d771baff2f51af705e85c4b1225
Julian Rosenblum <jsrmath@gmail.com>
2016-01-02 22:27:20 -0500

    Rewrote parser to be done in two passes.  Added support for untitled sections and lines of just chords.

--- parser.js
+++ parser.js
@@ -3,12 +3,16 @@
 // Dependencies: underscore.js, string.js
 
 var parser = (function () {
-  var parseFooterStartLine = function (state, line) {
+  var isChordLine = function (line) {
+    return S(line).startsWith(':');
+  }
+
+  var preParseFooterStartLine = function (state, line) {
     state.current.footer = true;
     return state;
   };
 
-  var parseMetaLine = function (state, line) {
+  var preParseMetaLine = function (state, line) {
     var prefix = line.split(' ')[0];
     var metaName = prefix.slice(2); // Remove ##
     var metaValue = S(line).chompLeft(prefix).trim().s;
@@ -17,9 +21,77 @@ var parser = (function () {
     return state;
   };
 
-  var parseSectionLine = function (state, line) {
-    var sectionName = S(line.slice(1)).trim().s; // Remove # and trim
+  var preParseFooterLine = function (state, line) {
+    var parts = S(line).strip(' ').split('=>');
+    var chord;
+    var alts;
 
+    if (parts.length !== 2) return state;
+
+    chord = parts[0];
+    alts = parts[1].split(',');
+
+    state.alts[chord] = alts;
+    return state;
+  };
+
+  var preParseTextLine = function (state, line) {
+    var lastLine = state.lines.length ? _.last(state.lines) : null;
+
+    // If we have a section header
+    if (S(line).startsWith('#')) {
+      state.lines.push({
+        type: 'section',
+        name: S(line.slice(1)).trim().s
+      });
+    }
+
+    // If we have a chord line
+    else if (isChordLine(line)) {
+      state.lines.push({
+        type: 'lyricChord',
+        chords: S(line.slice(1)).trim().s.split(' '),
+        lyrics: ''
+      });
+    }
+
+    // If we have a lyric line
+    else {
+      // If last line is a chord line, add lyrics to it
+      if (lastLine && lastLine.type === 'lyricChord' && !lastLine.lyrics) {
+        lastLine.lyrics = line;
+        return state;
+      }
+      // Otherwise, we have a solitary line of lyrics
+      else {
+        state.lines.push({
+          type: 'lyricChord',
+          chords: [],
+          lyrics: line
+        });
+      }
+    }
+
+    return state;
+  };
+
+  // Given a preparse state, preparses a line of Markato and returns updated state
+  var preParseLine = function (state, line) {
+    // Remove extraneous whitespace
+    line = S(line).trim().collapseWhitespace().s;
+
+    if (!line) return state;
+
+    if (S(line).startsWith('###')) return preParseFooterStartLine(state, line);
+
+    if (S(line).startsWith('##')) return preParseMetaLine(state, line);
+
+    if (state.current.footer) return preParseFooterLine(state, line);
+
+    return preParseTextLine(state, line);
+  };
+
+  var addSection = function (state, sectionName) {
     state.current.sectionName = sectionName;
 
     // It's a new section if we don't already have it on the list
@@ -39,150 +111,123 @@ var parser = (function () {
     return state;
   };
 
-  var parseChordLine = function (state, line) {
-    var chords = S(line.slice(1)).trim().s.split(' '); // Remove : and trim
-    var sectionName = state.current.sectionName;
 
+  var addLyricChord = function (state, lyrics, chords) {
+    var sectionName = state.current.sectionName;
+    var caratSplit = lyrics.split('^'); // Used to figure out phrases
+    var chordIndex = 0; // Index in list of chords as we assemble phrases
+    var phrases = [];
+    var exceptionIndices = []; // Chord indices that are exceptions in this section
+    var sectionChords;
+
+    var addPhrase = function (obj) {
+      obj = _.defaults(obj, {
+        string: '',
+        chord: '',
+        exception: false,
+        wordExtension: false
+      });
+
+      // Fetch alternates
+      obj.alts = state.alts[obj.chord] || [];
+      obj.chord = S(obj.chord).strip('\'').s;
+
+      phrases.push(obj);
+    };
+
+    // If we're not in a section, create one called untitled
+    if (sectionName === null) {
+      sectionName = 'UNTITLED'
+      state = addSection(state, sectionName);
+    }
+
+    // If this is a new section, add the chords to that section
     if (state.current.newSection) {
       state.chords[sectionName].push(chords);
     }
 
-    state.current.lastChords = chords;
+    // Get the chords stored for the section
+    sectionChords = state.chords[sectionName][state.current.line];
 
-    return state;
-  };
+    // If there is no above line of chords, use the section chords
+    if (!chords.length) chords = sectionChords
 
-  var parseFooterLine = function (state, line) {
-    var parts = S(line).strip(' ').split('=>');
-    var chord;
-    var alts;
+    // Substitute * from sectionChords where necessary
+    chords = _.map(chords, function (chord, index) {
+      // For *, get the chord at the same index from the chord list
+      if (chord === '*') return sectionChords[index];
 
-    if (parts.length !== 2) return state;
+      exceptionIndices.push(index);
+      return chord;
+    });
 
-    chord = parts[0];
-    alts = parts[1].split(',');
+    // If there are no lyrics, just add a line of chords
+    if (!lyrics) {
+      _.each(chords, function (chord) {
+        addPhrase({chord: chord});
+      });
+    }
+    // Otherwise, add lyrics based on carats
+    else {
+      _.each(caratSplit, function (phrase, index) {
+        var chord;
+        var lastPhrase;
 
-    // Add alternates for each chord and remove ' marks
-    _.each(state.lyrics, function (section) {
-      _.each(section.lines, function (line) {
-        _.each(line, function (phrase) {
-          if (phrase.chord === chord) {
-            phrase.alts = alts;
-            phrase.chord = S(phrase.chord).strip('\'').s;
+        // Special case first phrase
+        if (index === 0) {
+          if (phrase) {
+            addPhrase({string: caratSplit[0]});
           }
-        });
-      });
-    });
-
-    return state;
-  };
-
-  var parseLyricLine = function (state, line) {
-    var caratSplit = line.split('^'); // Used to figure out phrases
-    var chordIndex = 0; // Index in list of chords as we assemble phrases
-    var phrases = [];
-    var exceptionIndices = []; // Chord indices that are exceptions in this section
-
-    // Start off by assuming the chords are the current chords in this verse
-    var chords = state.chords[state.current.sectionName][state.current.line];
-
-    // If there are no current chords in this verse, add an empty array to the verse
-    if (!chords) {
-      chords = state.chords[state.current.sectionName][state.current.line] = [];
-    }
-
-    // If we're not in a new section and we just saw a line of chords, this means
-    // substitutions are being made in this section
-    if (!state.current.isNew && state.current.lastChords) {
-      chords = _.map(state.current.lastChords, function (chord, index) {
-        // For *, get the chord at the same index from the chord list
-        if (chord === '*') return chords[index];
-
-        exceptionIndices.push(index);
-        return chord;
-      });
-    }
-
-    _.each(caratSplit, function (phrase, index) {
-      var addPhrase = function (obj) {
-        phrases.push(_.defaults(obj, {
-          string: '',
-          chord: '',
-          exception: false,
-          alts: [],
-          wordExtension: false
-        }));
-      };
-
-      var chord;
-      var lastPhrase;
-
-      // Special case first phrase
-      if (index === 0) {
-        if (phrase) {
-          addPhrase({string: caratSplit[0]});
+          return;
         }
-        return;
-      }
 
-      lastPhrase = caratSplit[index - 1];
+        lastPhrase = caratSplit[index - 1];
 
-      // Get next chord
-      chord = chords[chordIndex];
+        // Get next chord
+        chord = chords[chordIndex];
 
-      // 'foo ^ bar' case, we insert the chord with a blank lyric
-      if (phrase && phrase[0] == ' ') {
-        addPhrase({
-          chord: chord,
-          exception: _.contains(exceptionIndices, chordIndex)
-        });
+        // 'foo ^ bar' case, we insert the chord with a blank lyric
+        if (phrase && phrase[0] == ' ') {
+          addPhrase({
+            chord: chord,
+            exception: _.contains(exceptionIndices, chordIndex)
+          });
 
-        addPhrase({
-          string: S(phrase).trim().s,
-        });
-      }
-      else {
-        addPhrase({
-          string: S(phrase).trim().s,
-          chord: chord,
-          exception: _.contains(exceptionIndices, chordIndex),
-        });
-      }
+          addPhrase({
+            string: S(phrase).trim().s,
+          });
+        }
+        else {
+          addPhrase({
+            string: S(phrase).trim().s,
+            chord: chord,
+            exception: _.contains(exceptionIndices, chordIndex),
+          });
+        }
 
-      // Check for foo^bar case (doesn't start with space and last phrase doesn't end with one)
-      if (phrase && phrase[0] !== ' ' && lastPhrase && S(lastPhrase).right(1).s !== ' ') {
-        _.last(phrases).wordExtension = true;
-      }
+        // Check for foo^bar case (doesn't start with space and last phrase doesn't end with one)
+        if (phrase && phrase[0] !== ' ' && lastPhrase && S(lastPhrase).right(1).s !== ' ') {
+          _.last(phrases).wordExtension = true;
+        }
 
-      chordIndex += 1;
-    });
+        chordIndex += 1;
+      });
+    }
     
     // Add new line to current lyric section
     _.last(state.lyrics).lines.push(phrases);
 
-    state.current.lastChords = null;
     state.current.line += 1;
     return state;
   };
 
   // Given a parse state, parses a line of Markato and returns updated state
   var parseLine = function (state, line) {
-    // Remove extraneous whitespace
-    line = S(line).trim().collapseWhitespace().s;
+    if (line.type === 'section') return addSection(state, line.name);
 
-    if (!line) return state;
+    if (line.type === 'lyricChord') return addLyricChord(state, line.lyrics, line.chords);
 
-    if (S(line).startsWith('###')) return parseFooterStartLine(state, line);
-
-    if (S(line).startsWith('##')) return parseMetaLine(state, line);
-
-    if (S(line).startsWith('#')) return parseSectionLine(state, line);
-
-    if (S(line).startsWith(':')) return parseChordLine(state, line);
-
-    if (state.current.footer) return parseFooterLine(state, line);
-
-    return parseLyricLine(state, line);
+    return state; // Should not ever reach this
   };
 
   // Given a finished parse state, return a markato object
@@ -194,22 +239,34 @@ var parser = (function () {
   return {
     parseString: function (str) {
       var lines = S(str).lines();
-      var state = {
+      var preParseState = {
         current: {
-          sectionName: '', // Current section we're in
+          footer: false // Are we in the footer?
+        },
+        meta: {},
+        alts: {},
+        lines: []
+      };
+      var parseState = {
+        current: {
+          sectionName: null, // Current section we're in
           newSection: true, // Is this a section being newly defined?
-          line: 0, // What line of the section are we in (incremented on parseLyric)
-          footer: false, // Are we in the footer?
-          lastChords: null // Array of last line of chords we've seen
+          line: 0 // What line of the section are we in (incremented on parseLyric)
         },
         sections: [],
         chords: {},
-        lyrics: [],
-        meta: {}
+        lyrics: []
       };
 
-      state = _.reduce(lines, parseLine, state);
-      return markatoObjectFromState(state);
+      // Run preparser
+      preParseState = _.reduce(lines, preParseLine, preParseState);
+
+      // Extract relevant information from preParseState
+      parseState.meta = preParseState.meta;
+      parseState.alts = preParseState.alts;
+
+      parseState = _.reduce(preParseState.lines, parseLine, parseState);
+      return markatoObjectFromState(parseState);
     }
   };
 }());
