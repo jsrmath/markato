0831f10a2db030d450d081cc65871ba2c6498dcd
Julian Rosenblum <jsrmath@gmail.com>
2016-08-25 11:59:04 -0400

    Added prod specific build and gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ var gulp = require('gulp');
 var del = require('del');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
+var shell = require('gulp-shell');
 
 gulp.task('build', function () {
-  gulp.src('public/scripts/main.cjsx', { read: false })
+  return gulp.src('public/scripts/main.cjsx', { read: false })
     .pipe(browserify({
       transform: ['coffee-reactify'],
       extensions: ['.coffee', '.cjsx']
@@ -13,4 +14,14 @@ gulp.task('build', function () {
     .pipe(gulp.dest('public'))
 });
 
+gulp.task('apply-prod-environment', function() {
+  return process.env.NODE_ENV = 'production';
+});
+
 gulp.task('default', ['build']);
+
+gulp.task('production', ['apply-prod-environment', 'build']);
+
+gulp.task('deploy', ['production'], shell.task([
+  'firebase deploy'
+]));
