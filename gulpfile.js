cdb55c7079465288852917df6590c7af9543a480
Julian Rosenblum <jsrmath@gmail.com>
2016-08-26 11:00:58 -0400

    Added uglify to production builds

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var del = require('del');
 var browserify = require('gulp-browserify');
 var rename = require('gulp-rename');
 var shell = require('gulp-shell');
+var uglify = require('gulp-uglify');  
 
 gulp.task('build', function () {
   return gulp.src('public/scripts/main.cjsx', { read: false })
@@ -14,13 +15,19 @@ gulp.task('build', function () {
     .pipe(gulp.dest('public'))
 });
 
+gulp.task('uglify', ['build'], function () {
+  return gulp.src('public/bundle.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('public'))
+});
+
 gulp.task('apply-prod-environment', function() {
   return process.env.NODE_ENV = 'production';
 });
 
 gulp.task('default', ['build']);
 
-gulp.task('production', ['apply-prod-environment', 'build']);
+gulp.task('production', ['apply-prod-environment', 'build', 'uglify']);
 
 gulp.task('deploy', ['production'], shell.task([
   'firebase deploy'
